50bfa2538cecf020fdd5b7980d04e49148c9680a
ludasco <ludasco79@gmail.com>
2026-04-07 11:39:20 -0400

    RT

--- aws/payer/vpc.tf
+++ aws/payer/vpc.tf
@@ -32,3 +32,24 @@ resource "aws_subnet" "private" {
     Environment = var.environment
   }
 }
+
+# Main Route Table
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name        = "main-route-table"
+    Environment = var.environment
+  }
+}
+
+# Route Table Associations
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.main.id
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.main.id
+}
