e48f4bed09c2c002d1f0431c69d0f808b367857f
ludasco <ludasco79@gmail.com>
2026-04-07 09:54:15 -0400

    updates

--- aws/payer/vpc.tf
+++ aws/payer/vpc.tf
@@ -1,11 +1,11 @@
 resource "aws_vpc" "main" {
-  cidr_block           = "10.3.0.0/16"
+  cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
   enable_dns_support   = true
 
   tags = {
     Name        = "terraform-course"
-    Environment = "lab-learning"
+    Environment = var.environment
     Managed_by  = "Terraform"
   }
 }
